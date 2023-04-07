90d921c8bc48bc2c3c26054aaf72fbab4e5dfac5
F P <florian@pfleiderer.at>
2023-04-07 14:04:12 +0300

    added display layout like notability sctech

--- api/api.py
+++ api/api.py
@@ -1,21 +1,25 @@
-from flask import Flask, request,jsonify
+from flask import Flask, request, jsonify
 from products import Inventory
 
 app = Flask(__name__)
-coffee1 = Inventory.testCoffee1
-coffee2 = Inventory.testCoffee2
+inventory = []
 
-coffees = {'coffee1': coffee1, 'coffee2': coffee2}
 
-@app.route('/get_coffee')
-def get_coffee():
-    return {'coffee1': {'origin': coffees['coffee1'].origin, 'farmer': coffees['coffee1'].farmer, 'price': coffees['coffee1'].price},
-            'coffee2': {'origin': coffees['coffee2'].origin, 'farmer': coffees['coffee2'].farmer, 'price': coffees['coffee2'].price}}
+@app.route('/api/coffees', methods=['POST'])
+def add_coffee():
+    coffee_data = request.get_json()
+    inventory.append(coffee_data)
+    return 'Coffee added', 200
 
-@app.route('/post_price', methods=['POST', 'GET'])
-def receive_price():
-    if request.method == 'POST':
-        coffees['coffee1'].price = request.json['data']
-        return {'message': 'Price received'}
-    return {'message': 'no price received'}
-    
+
+# @app.route('/get_coffee')
+# def get_coffee():
+#     return {'coffee1': {'origin': coffees['coffee1'].origin, 'farmer': coffees['coffee1'].farmer, 'price': coffees['coffee1'].price},
+#             'coffee2': {'origin': coffees['coffee2'].origin, 'farmer': coffees['coffee2'].farmer, 'price': coffees['coffee2'].price}}
+
+# @app.route('/post_price', methods=['POST', 'GET'])
+# def receive_price():
+#     if request.method == 'POST':
+#         coffees['coffee1'].price = request.json['data']
+#         return {'message': 'Price received'}
+#     return {'message': 'no price received'}
--- src/App.js
+++ src/App.js
@@ -1,124 +1,151 @@
-import React, {useState} from 'react';
-// import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 
 function MainPage() {
-    //get price
-    const [currentPrice, setCurrentPrice] = useState(0);
-    const [currentFarmer, setFarmer] = useState('');
-    const [data, setData] = useState('');
-    // const [data1, setData1] = useState('');
+  //get price
+  const [currentPrice, setCurrentPrice] = useState(0);
+  const [currentFarmer, setFarmer] = useState('');
+  const [data, setData] = useState('');
 
-    function postPrice() {
-      fetch('/post_price', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({data})
-      })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error(error));
-    };
+  const coffees = [
+    { name: 'Coffee 1', price: 2.50, farmer: 'Farmer 1' },
+    { name: 'Coffee 2', price: 3.00, farmer: 'Farmer 2' },
+    { name: 'Coffee 3', price: 2.75, farmer: 'Farmer 3' },
+  ];
 
-    async function handleLeftButtonClick() {
-      const response = await fetch('/get_coffee');
-      const data = await response.json();
-      const newWindow = window.open('');
-      newWindow.document.write(`<pre>${JSON.stringify(data, null, 2)}</pre>`);
-      newWindow.document.write(`
-        <div>
-          <button onclick="window.close()">Go back to main page</button>
-        </div>
-      `); 
-    
-    };
+  const [showTable, setShowTable] = useState(false);
+  const [coffeeName, setCoffeeName] = useState('');
+  const [coffeePrice, setCoffeePrice] = useState('');
+  const [coffeeFarmer, setCoffeeFarmer] = useState('');
 
-  // middle button displays price and farmer
-  function handleMiddleButtonClick() {
-    fetch('/get_coffee').then(res => res.json()).then(data => {
-      setCurrentPrice(data.coffee1.price);
-      setFarmer(data.coffee1.farmer);
+  const toggleTable = () => {
+    setShowTable(!showTable);
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const newCoffee = { name: coffeeName, price: coffeePrice, farmer: coffeeFarmer };
+    const response = await fetch('/api/coffees', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newCoffee)
     });
-  };
+    if (response.ok) {
+      setCoffeeName('');
+      setCoffeePrice('');
+      setCoffeeFarmer('');
+      setShowTable(true);
+    } else {
+      // handle error
+    }
+  }
 
-  // write button is for writing data
-  function handleRightButtonClick() {
-    postPrice();
-  };
+  // function postPrice() {
+  //   fetch('/post_price', {
+  //     method: 'POST',
+  //     mode: 'cors',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({ data })
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => console.log(data))
+  //     .catch(error => console.error(error));
+  // };
+
+  // async function handleLeftButtonClick() {
+  //   const response = await fetch('/get_coffee');
+  //   const data = await response.json();
+  //   const newWindow = window.open('');
+  //   newWindow.document.write(`<pre>${JSON.stringify(data, null, 2)}</pre>`);
+  //   newWindow.document.write(`
+  //       <div>
+  //         <button onclick="window.close()">Go back to main page</button>
+  //       </div>
+  //     `);
+
+  // };
+
+  // // middle button displays price and farmer
+  // function handleMiddleButtonClick() {
+  //   fetch('/get_coffee').then(res => res.json()).then(data => {
+  //     setCurrentPrice(data.coffee1.price);
+  //     setFarmer(data.coffee1.farmer);
+  //   });
+  // };
+
+  // // write button is for writing data
+  // function handleRightButtonClick() {
+  //   postPrice();
+  // };
 
   return (
-    <div>
-      <p>The current price is {currentPrice}.</p>
-      <p>The current farmer is {currentFarmer}.</p>
-      <p><input value={data} onChange={e => setData(e.target.value)} /></p>
-      
-      <button onClick={handleLeftButtonClick}>MY COFFEES</button>
-      <button onClick={handleMiddleButtonClick}>Daten aktualisieren</button>
-      <button onClick={handleRightButtonClick}>POST</button>
+    <div className="App">
+      <header>
+        <h1>Great Coffee App</h1>
+      </header>
+      <main>
+        {showTable && (
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Farmer</th>
+              </tr>
+            </thead>
+            <tbody>
+              {coffees.map((coffee, index) => (
+                <tr key={index}>
+                  <td>{coffee.name}</td>
+                  <td>{coffee.price}</td>
+                  <td>{coffee.farmer}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={coffeeName} onChange={(event) => setCoffeeName(event.target.value)} />
+          </label>
+          <label>
+            Price:
+            <input type="text" value={coffeePrice} onChange={(event) => setCoffeePrice(event.target.value)} />
+          </label>
+          <label>
+            Farmer:
+            <input type="text" value={coffeeFarmer} onChange={(event) => setCoffeeFarmer(event.target.value)} />
+          </label>
+          <button type="submit">Add Coffee</button>
+        </form>
+      </main>
+      <footer>
+        <button>Explore</button>
+        <button>Brew</button>
+        <button onClick={toggleTable}>My Coffee</button>
+      </footer>
     </div>
+    // <div className="phone">
+    //   <div className="screen">
+    //     {/* Add your app's content here */}
+    //     <p>The current price is {currentPrice}.</p>
+    //     <p>The current farmer is {currentFarmer}.</p>
+    //     <p><input value={data} onChange={e => setData(e.target.value)} /></p>
+    //   </div>
+    //   <div className="buttons">
+    //     <button onClick={handleLeftButtonClick}>MY COFFEES</button>
+    //     <button onClick={handleMiddleButtonClick}>Daten aktualisieren</button>
+    //     <button onClick={handleRightButtonClick}>POST</button>
+    //   </div>
+    // </div>
   );
 }
 
 export default MainPage;
 
-/*
-function App() {
-  //get price
-  const [currentPrice, setCurrentPrice] = useState(0);
-  const [currentFarmer, setFarmer] = useState('');
-
-  useEffect(() => {
-    fetch('/get_coffee').then(res => res.json()).then(data => {
-      setCurrentPrice(data.price);
-      setFarmer(data.farmer);
-    });
-  }, []);
-
-  //adds the option to post something to the api with a button
-  const [data, setData] = useState('');
-
-  function sendData() {
-  
-    fetch('/post_price', {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({data})
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current price is {currentPrice}.</p>
-        <p>The current farmer is {currentFarmer}.</p>
-        <input value={data} onChange={e => setData(e.target.value)} />
-        <button onClick={sendData}>Send Data</button>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-*/
