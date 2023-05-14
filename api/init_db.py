084384c1b3920e9b1bbd65ac0005fd8d98dd1f0c
florianpfleiderer <florian@pfleiderer.at>
2023-05-14 23:25:45 +0200

    added some data

--- api/init_db.py
+++ api/init_db.py
@@ -2,7 +2,7 @@
 """
 
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import sessionmaker, relationship, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 engine = create_engine('sqlite:///test.db')
@@ -20,13 +20,13 @@ class CoffeeDB(Base):
         farmer (str): the farmer of the coffee
         
     '''
-
     __tablename__ = 'coffee'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     price = Column(Integer, nullable=False)
     farmer = Column(String, nullable=False)
     # TODO add rest of the attributes
+    # TODO add a one to many relationship with BrewRecipes
 
 
 class GrinderDB(Base):
@@ -41,16 +41,16 @@ class GrinderDB(Base):
         burr (str): the burrs inside the grinder
         
     '''
-
     __tablename__ = 'grinder'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     price = Column(Integer, nullable=False)
     burr = Column(String, nullable=False)
     # TODO add rest of the attributes
+    # TODO add a one to many relationsship with BrewRecipes
 
 
-def BrewRecipes(Base):
+class BrewRecipes(Base):
     '''This class represents the table with all the brew recipes in the database.
     
     The columns (= attributes) are the recipes, and are connected to the other
@@ -63,13 +63,36 @@ def BrewRecipes(Base):
         coffee (str): the coffee used in the brew recipe
         grinder (str): the grinder used in the brew recipe
     '''
-
     __tablename__ = 'brew_recipes'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     method = Column(String, nullable=False)
     coffee = Column(String, ForeignKey('coffee.name'), nullable=False)
     grinder = Column(String, ForeignKey('grinder.name'), nullable=False)
+    # TODO add a backpopulated relationship with CoffeeDB and GrinderDB for recipes
     # TODO how to add the recipe itself (a script that declares the
     # duration, stops, etc to be executed by the BrewSection when loaded?)
 
+Base.metadata.create_all(engine)
+
+testCoffee1 = CoffeeDB(name='testCoffee1', price=12, farmer='Max')
+testCoffee2 = CoffeeDB(name='testCoffee2', price=14, farmer='Max')
+coffees = [testCoffee1, testCoffee2]
+
+fellowOde = GrinderDB(name='Fellow ode', burr='sspRedSpeed', price=350)
+wilfaUniform = GrinderDB(name='Wilfa Uniform', burr='Wilfa Standard Burrs', price='250')
+grinders = [fellowOde, wilfaUniform]
+
+recipeV60 = BrewRecipes(name='V60', method='pour over', coffee=testCoffee1, 
+                        grinder=fellowOde)
+recipeAeropress = BrewRecipes(name='Aeropress', method='immersion', coffee=testCoffee2, 
+                              grinder=wilfaUniform)
+recipes = [recipeV60, recipeAeropress]
+
+Session = sessionmaker(bind=engine)
+
+with Session() as session:
+    for coffee in coffees:
+        session.add(coffee)
+    # only one thread supported (siehe github issue)
+    session.commit()
