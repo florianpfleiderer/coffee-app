2aea8a3a4528777c7d4bb80fa83e8437e9db0790
florianpfleiderer <florian@pfleiderer.at>
2023-05-18 15:39:36 +0200

    refactored code to pylint standards

--- products/inventory_objects.py
+++ products/inventory_objects.py
@@ -23,22 +23,22 @@ class InventoryObjects:
     @classmethod
     def from_json(cls, json):
         ''' creates a new instance of an Object from a json object
-        
+
         Args:
             json (dict): a json object with the attributes of the Object
-            
+
         Returns:
             Object: a new instance of the sepcified inventory object
         '''
         return cls(**json)
- 
+
     def __str__(self):
         return f'{self.name} costs {self.price}€'
- 
+
 
 class Coffee(InventoryObjects):
     '''represents an Instance of a Coffee Variety
-    
+
     Attributes:
         origin (str): 
         variety (str): 
@@ -50,7 +50,8 @@ class Coffee(InventoryObjects):
             e.g.: 1.6 equals 1€ and 60 cents
     '''
 
-    def __init__(self, name=None, origin=None, variety=None, process=None, roast=None, farmer=None,size=None, price=None) -> None:
+    def __init__(self, name=None, origin=None, variety=None, process=None, roast=None,
+                 farmer=None, size=None, price=None) -> None:
         super().__init__(name, price)
         self.origin = origin
         self.variety = variety
@@ -76,10 +77,10 @@ class Coffee(InventoryObjects):
         '''
 
         return self.price / (self.size * 0.1)
-    
+
     def __str__(self):
         return f'{self.name} costs {self.price}€'
-    
+
 
 class Grinder(InventoryObjects):
     '''represents a Coffee Grinder
@@ -93,21 +94,23 @@ class Grinder(InventoryObjects):
     def __init__(self, name, burrs, price):
         super().__init__(name, price)
         self.burrs = burrs
-    
+
     def __str__(self):
         return f'{self.name} costs {self.price}€'
-    
+
 
 class Burr(InventoryObjects):
     '''represents a specific burr type
 
-    this class is used to filter out for burrtype or burr size as you can extract these data from this class
+    this class is used to filter out for burrtype or burr size as you can 
+    extract these data from this class
 
         args:
         name
-        type: this can either be flat or conical (if smething else as input -> set type None)
+        type: this can either be flat or conical (if smething else as input 
+            -> set type None)
         size: in mm
-        price: 
+        price:
     '''
 
     def __init__(self, name=None, type=None, size=None, price=None):
@@ -119,7 +122,7 @@ class Burr(InventoryObjects):
 
     def __str__(self):
         return f'{self.name} costs {self.price}€'
-    
+
 
 #recipe
 class Recipe(InventoryObjects):
@@ -141,10 +144,10 @@ class Recipe(InventoryObjects):
         totalTime (int): the time in seconds till the coffee is ready
         Pour1stTime (int): the time in seconds till the first pour is finished
         Pour2ndTime (int): the time in seconds till the second pour is finished
-
     '''
 
-    def __init__(self, name=None, coffee='Choose a Coffee', grinder='Choose a Grinder', water=0, coffeeIn=0, ratio=0, temp=0, totalTime=0, Pour1stTime=0, Pour2ndTime=0, price=0):
+    def __init__(self, name=None, coffee='Choose a Coffee', grinder='Choose a Grinder', water=0,
+                 coffeeIn=0, ratio=0, temp=0, totalTime=0, Pour1stTime=0, Pour2ndTime=0, price=0):
         super().__init__(name, price)
         self.coffee = coffee
         self.grinder = grinder
@@ -156,18 +159,9 @@ class Recipe(InventoryObjects):
         self.Pour1stTime = Pour1stTime
         self.Pour2ndTime = Pour2ndTime
 
-    
+
     def __str__(self):
         return f'{self.name} costs {self.price}€'
-    
-
-
-
-
-
-
-
-
 
 # class CoffeeMachine(InventoryObjects):
 #     '''represents a Coffee Machine
@@ -181,10 +175,9 @@ class Recipe(InventoryObjects):
 #     def __init__(self, name, type, price):
 #         super().__init__(name, price)
 #         self.type = type
-    
+
 #     def __str__(self):
 #         return f'{self.name} costs {self.price}€'
-    
 
 # class Accessory(InventoryObjects):
 #     '''represents an Accessory for Coffee
@@ -198,10 +191,7 @@ class Recipe(InventoryObjects):
 #     def __init__(self, name, type, price):
 #         super().__init__(name, price)
 #         self.type = type
-    
+
 #     def __str__(self):
 #         return f'{self.name} costs {self.price}€'
-    
-    
-
-    
+ 
